9cae09557886dc427d88af595250334d65d61ff4
ashish-choudhari-git <contact.ashishchoudhari@gmail.com>
2025-08-17 04:29:59 +0530

    Solved one LL question

---
+++ LinkedList/FindLastInLL.java
@@ -0,0 +1,64 @@
+package LinkedList;
+    
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    Node head;
+
+    public void insert(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+
+    public Node getLastNode() {
+        if (head == null) {
+            return null;
+        }
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        return current;
+    }
+
+    
+    public void display() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+}
+public class FindLastInLL {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        list.insert(10);
+        list.insert(20);
+        list.insert(30);
+        list.insert(40);
+        list.display();
+        Node last = list.getLastNode();
+        System.out.println("Last node is: " + last.data);
+        
+    }
+}
